48a8cb16a52ca3cfaa5c235b392a5eb8a389e61f
Max Bernasconi <max.bernasconi86@gmail.com>
2023-10-02 20:06:37 -0300

    layaouts for guests and components corrected

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -17,8 +15,6 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
@@ -27,7 +23,6 @@
                     </div>
                 </header>
             @endif
-
             <!-- Page Content -->
             <main class="min-h-screen min-w-full bg-gray-900">
                 {{ $slot }}
